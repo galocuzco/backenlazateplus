52e4effdb8ad5ea33a05cdbd654440dd86a196f1
galoc <galo.cuzco@hotmail.com>
2023-04-21 22:22:20 -0500

    Revert "Revert "Revert "commit incial de proyecto 21-abril"""
    This reverts commit a481d79ba83eec4f029649f07ac5c0178206252c.

--- app-enlazateplus/src/main/java/com/enlazateplus/app/service/interfaces/RestauranteService.java
+++
@@ -1,9 +0,0 @@
-package com.enlazateplus.app.service.interfaces;
-
-import com.enlazateplus.app.dto.RestauranteDTO;
-import com.enlazateplus.app.service.interfaces.general.GenericService;
-
-public interface RestauranteService extends GenericService<RestauranteDTO>
-{
-	
-}
