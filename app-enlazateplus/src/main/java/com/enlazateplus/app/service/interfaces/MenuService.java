d6f0e7d432ce6a8666d2481e6b0a2dbab0d4e2a7
galoc <galo.cuzco@hotmail.com>
2023-04-21 22:17:14 -0500

    Revert "commit incial de proyecto 21-abril"
    This reverts commit e64b7cb5905100d377cbd069ea32d08423c7212b.

--- app-enlazateplus/src/main/java/com/enlazateplus/app/service/interfaces/MenuService.java
+++
@@ -1,9 +0,0 @@
-package com.enlazateplus.app.service.interfaces;
-
-import com.enlazateplus.app.dto.MenuDTO;
-import com.enlazateplus.app.service.interfaces.general.GenericService;
-
-public interface MenuService extends GenericService<MenuDTO>
-{
-
-}
